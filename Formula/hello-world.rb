6f8e0b4b8968dbd11196c41e32f6e2515c0e4df3
Liu Qian <112486391@qq.com>
2019-12-03 22:17:16 +0800

    update

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -2,15 +2,15 @@ class HelloWorld < Formula
   desc "hello world demo"
   url "https://github.com/l19861225q/homebrew-6k", :using => :git
 
-  # depends_on "cmake" => :build
+  depends_on "cmake" => :build
 
   def install
-    system "g++", "./hello-world.cpp", "-o", "hello-world"
-    bin.install "hello-world"
-    # mkdir "build" do
-    #   system "cmake", ".."
-    #   system "make"
-    # end
-    # bin.install hello-world
+    # system "g++", "./hello-world.cpp", "-o", "hello-world"
+    # bin.install "hello-world"
+    mkdir "build" do
+      system "cmake", ".."
+      system "make"
+      bin.install "hello-world"
+    end
   end
 end
